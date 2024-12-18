43ecb8e7ea5a798b9373bcb73e8bdd5dc5c5c7f5
bkrusche <benjamin.krusche@clarity.ai>
2024-12-18 22:31:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -143,10 +143,11 @@ def main():
             for module in selected_modules:
                 module_row = modules_df[modules_df['Product module'] == module].iloc[0]
                 for method, selected in selected_access_methods.items():
+                    st.write(f"- {module}, {method}, {module_row[method]} ")
                     # Ensure proper handling of availability column (True or False)
                     if selected and module_row[method] == "FALSE":  # Check the availability (TRUE or FALSE)
                         incompatible_combinations.append((module, method))
-                        st.write(f"- {module} is not available with {method}")
+                        
             
             # Display incompatible combinations
             if incompatible_combinations:
