41a9d388d23fe5707f9973d687e7b80eab4aa0d0
bkrusche <benjamin.krusche@clarity.ai>
2024-12-18 22:34:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -145,8 +145,9 @@ def main():
                 for method, selected in selected_access_methods.items():
                     st.write(f"- {selected}, {module}, {method}, {module_row[method]} ")
                     # Ensure proper handling of availability column (True or False)
-                    if selected =="TRUE" and module_row[method] == "FALSE":  # Check the availability (TRUE or FALSE)
-                        incompatible_combinations.append((module, method))
+                    if selected == "TRUE" 
+                        if module_row[method] == "FALSE":  # Check the availability (TRUE or FALSE)
+                            incompatible_combinations.append((module, method))
                         
             
             # Display incompatible combinations
